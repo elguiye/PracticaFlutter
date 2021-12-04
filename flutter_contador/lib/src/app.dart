da48af98fb52e05063b244114dc391a02577a67a
el_guiye <gobregonortiz@gmail.com>
2021-12-04 13:23:06 -0300

    Implementando widgets -Scaffold -

--- flutter_contador/lib/src/app.dart
+++ flutter_contador/lib/src/app.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_contador/src/pages/home-page.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(context) {
     return MaterialApp(
-      home: Center(child: Text('Hola Mundo')),
+      home: HomePage(),
     );
   }
 }
